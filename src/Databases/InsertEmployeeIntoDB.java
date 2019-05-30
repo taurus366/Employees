07357e97254dd0dcc76234958f657ca066076c99
ALI ZINAL <taurus.ali47@gmail.com>
2019-05-30 17:51:20 +0300

    changed file places

---
+++ src/Databases/InsertEmployeeIntoDB.java
@@ -0,0 +1,47 @@
+package Databases;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class InsertEmployeeIntoDB {
+
+	public static void insertIntoDatabase(String myData) {
+
+		String database_file_location = "W:\\Users\\newArcerr\\Desktop\\employees.txt";
+		File txtFile = new File(database_file_location);
+		if (!txtFile.exists()) {
+			try {
+				File directory = new File(txtFile.getParent());
+				if (!directory.exists()) {
+					directory.mkdirs();
+				}
+				txtFile.createNewFile();
+			} catch (IOException e) {
+				log("Excepton Occured: " + e.toString());
+			}
+		}
+
+		try {
+			txtFile.delete();
+
+			// Convenience class for writing character files
+			FileWriter academyWriter;
+			academyWriter = new FileWriter(txtFile.getAbsoluteFile(), true);
+
+			// Writes text to a character-output stream
+			BufferedWriter bufferWriter = new BufferedWriter(academyWriter);
+			bufferWriter.write(myData.toString());
+			bufferWriter.close();
+
+			log("Employee saved at file location: " + database_file_location + " Data: " + myData + "\n");
+		} catch (IOException e) {
+			log("Hmm.. Got an error while saving Employee data to file " + e.toString());
+		}
+	}
+
+	public static void log(String string) {
+		System.out.println(string);
+	}
+}
--- src/main/StartHERE.java
+++ src/main/StartHERE.java
@@ -3,25 +3,41 @@ package main;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.google.gson.Gson;
+
+import Databases.InsertEmployeeIntoDB;
+import console.AddEmployee;
 import models.Employees;
 
 public class StartHERE {
 	public static ArrayList<Employees> employees = new ArrayList<>();
 	public static Scanner keyboard;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Gson gson = new Gson();
+		AddEmployee.AddEmployees();
 		
-		AddEmployee.AddEmployee1();
-		
+
 		keyboard = new Scanner(System.in);
-		
+
 		System.out.println("Do you want these employees add to Json list ? yes/no");
 		String answer = keyboard.next();
-		
-		if(answer.equals("yes")) {
-			System.out.println("FUCK OF YOU!");
+
+		if (answer.equals("yes")) {
+			System.out.println(employees.size());
+			
+			InsertEmployeeIntoDB.insertIntoDatabase(gson.toJson(employees));
+
 		}
 		
+		System.out.println("Do you want to read Json list from file ? yes/no");
+		String answer2 = keyboard.next();
+		
+		if(answer2.equals("yes")) {
+			
+		}
+
 	}
 
 }
