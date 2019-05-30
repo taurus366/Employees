a8fbd5a0fdc1fb848b567b13bb4d507acbbddab3
taurus366 <taurus.ali47@gmail.com>
2019-05-30 13:36:37 +0300

    istrue = true;

--- src/main/Server.java
+++ src/main/Server.java
@@ -12,15 +12,37 @@ public class Server {
 		// TODO Auto-generated method stub
 		keyboard = new Scanner(System.in);
 		ArrayList<Employees> employees = new ArrayList<>();
-		 
+		int employee1totalSales=0;
 		
 		do {
 			System.out.println("Please enter your name: ");
 			String employee1name = keyboard.next();
-			System.out.println("Please enter employee totalSales: ");
-			int employee1totalSales = keyboard.nextInt();
+			boolean istrue = true;
+			do {
+				System.out.println("Please enter employee totalSales: ");
+//				if(keyboard.hasNextInt()) {
+//					employee1totalSales=keyboard.nextInt();
+//					istrue = true;
+//				}else {
+//					istrue = false;
+//					keyboard.nextLine();
+//				}
+				try {
+				employee1totalSales = keyboard.nextInt();
+				istrue = true;
+				} catch (Exception e) {
+					// TODO: handle exception
+					istrue=false;
+					keyboard.nextLine();
+				}
+				
+			} while (!istrue == true);
+			//System.out.println("Please enter employee totalSales: ");
+			//int employee1totalSales = keyboard.nextInt();
+			
 			System.out.println("Please enter employee salesPeriod: ");
 			int employee1salesPeriod = keyboard.nextInt();
+			
 			System.out.println("Please enter employee experienceMultiplier: ");
 			double employee1experienceMultiplier = keyboard.nextDouble();
 		
