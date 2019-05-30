34a7f4e77ce8407973a5f56e1ab23d7e1e0333b8
taurus366 <taurus.ali47@gmail.com>
2019-05-30 11:54:00 +0300

    yes/no question is done!

--- src/main/Server.java
+++ src/main/Server.java
@@ -14,25 +14,23 @@ public class Server {
 		ArrayList<Employees> employees = new ArrayList<>();
 		 
 		
-		System.out.println("Please enter your name: ");
-		String employee1name = keyboard.nextLine();
-		System.out.println("Please enter employee totalSales: ");
-		int employee1totalSales = keyboard.nextInt();
-		System.out.println("Please enter employee salesPeriod: ");
-		int employee1salesPeriod = keyboard.nextInt();
-		System.out.println("Please enter employee experienceMultiplier: ");
-		double employee1experienceMultiplier = keyboard.nextDouble();
-		//employees.add(employee1name+employee1totalSales+employee1salesPeriod+employee1experienceMultiplier);
-		//employees.add(employee1name);
-		//Companies companies1 = new Companies(txtCompanyname.getText(), txtPosition.getText(),txtType.getText(), dtrpnDescription.getText());
-		//companies.add(companies1);
-		Employees employee = new Employees(employee1name, employee1totalSales, employee1salesPeriod, employee1experienceMultiplier);
-		employees.add(employee);
-		System.out.println(employees.size());
-		for(int i=0;i<employees.size();i++) {
-			Employees ik = employees.get(i);
-			System.out.println(ik.name+ik.salesPeriod+ik.totalSales+ik.experienceMultiplier);
-		}
+		do {
+			System.out.println("Please enter your name: ");
+			String employee1name = keyboard.next();
+			System.out.println("Please enter employee totalSales: ");
+			int employee1totalSales = keyboard.nextInt();
+			System.out.println("Please enter employee salesPeriod: ");
+			int employee1salesPeriod = keyboard.nextInt();
+			System.out.println("Please enter employee experienceMultiplier: ");
+			double employee1experienceMultiplier = keyboard.nextDouble();
+		
+			Employees employee = new Employees(employee1name, employee1totalSales, employee1salesPeriod, employee1experienceMultiplier);
+			employees.add(employee);
+			System.out.println("Do you want to add more? yes/no");
+			
+			
+		} while (keyboard.next().equals("yes"));
+		
 		
 	}
 
